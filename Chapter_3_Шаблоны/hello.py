fab7a796fa352c9194798872e4af834175b1b132
milovanov <milovanov160386@gmail.com>
2021-01-22 17:07:50 +0300

    Закончил главу 3

--- Chapter_3_Шаблоны/hello.py
+++ Chapter_3_Шаблоны/hello.py
@@ -1,18 +1,32 @@
-from flask import Flask
-from flask.etx.bootstrap import Bootstrap
+from datetime import datetime
+
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello World !</h1>"
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return f"<h1>Hello, {name} </h1>"
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
